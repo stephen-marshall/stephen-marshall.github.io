bcb236655b76935020fc9196b0f486a4fa3302b8
stephen-marshall <stephen.marshall@freshworks.com>
2022-02-10 22:55:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,15 @@
-fetch('https://stephen-marshall.github.io/config-data.json')
-.then((data) => data.json())
-.then((data) => {
-  window.iframeWindow.getConfig(data);
+// fetch('https://stephen-marshall.github.io/config-data.json')
+// .then((data) => data.json())
+// .then((data) => {
+//   window.iframeWindow.getConfig(data);
+// });
+
+async function fetchConfig() {
+  const response = await fetch('https://stephen-marshall.github.io/config-data.json');
+  const config = await response.json();
+  return config;
+}
+fetchConfig().then(config => {
+  console.log(window.iframeWindow);
+console.log(window.iframeWindow.getConfig);
 });
