f8e25464a74887aa864a3647baf424a88f514bc2
stephen-marshall <stephen.marshall@freshworks.com>
2022-02-13 17:03:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-fetch('https://stephen-marshall.github.io/config-data.json')
+fetch('https://configdemochecks.s3.us-east-2.amazonaws.com/config-data.json')
 .then((data) => data.json())
 .then((data) => {
   window.parent.postMessage(data);
